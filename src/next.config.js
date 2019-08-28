a300e0fc8675c1f0f284704dd7361a8cbe4ef2a6
ps_fedotov <ps_fedotov@bingo-boom.com>
2019-08-28 20:01:13 +0300

    add baseURL

--- src/next.config.js
+++ src/next.config.js
@@ -1,4 +1,7 @@
+const baseUrl = require('./configs/base_url').baseUrl || '';
+
 module.exports = {
+  assetPrefix: baseUrl,
   webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
--- src/server.js
+++ src/server.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const next = require('next');
-const nextRoutes = require('next-routes');
-
+const baseUrl = require('./configs/base_url').baseUrl || '';
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-
-const routes = (module.exports = nextRoutes());
-routes.add('index', '*');
+const routes = require('./routes');
 
 const handle = routes.getRequestHandler(app, ({ req, res, route, query }) => {
-  return app.renderToHTML(req, res, route, query);
+  app.renderToHTML(req, res, route, query);
 });
 
 app.prepare().then(() => {
   const server = express();
-
-  server.get('*', (req, res) => {
-    return handle(req, res)
+  const appRouter = express.Router();
+  appRouter.get('/cms', (req, res) => {
+    res.status(200).send("Will be CMS!");
   });
-
+  server.use(baseUrl, appRouter);
+  server.use(baseUrl, handle);
   server.listen(port, err => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
